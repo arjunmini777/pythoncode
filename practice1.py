6a0790a721b1b01fb242ba393f0ddd29df7a2636
arjunmini777 <arjunmini777@gmail.com>
2025-10-27 20:39:26 -0400

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,10 +1,10 @@
-# Write a script that adds 15 and 30 and divides the sum by 2. Print out the answer.
+# 1. Write a script that adds 15 and 30 and divides the sum by 2. Print out the answer.
 a = 15
 b = 30
 c = (a + b)/2
 print(c)
 
-# Initialize two variables and use arithmetic operators to add, subtract, multiply, divide, and find the remainder.
+# 2. Initialize two variables and use arithmetic operators to add, subtract, multiply, divide, and find the remainder.
 a = 20
 b = 10
 print(f"adding a + b is equal to {a+b}")
@@ -13,6 +13,20 @@ print(f"multiplying a * b is equal to {a*b}")
 print(f"dividing a / b is equal to {a/b}")
 print(f"The remainder of a % b is equal to {a%b}")
 
-# Create a variable called name and store your name in it as a string.
+# 3. Create a variable called name and store your name in it as a string.
 name = "Arjun"
 print(name)
+
+# 4. Create three variables in one line and assign each one a different food item.
+
+# 5. Print out "Hello" ten times using arithmetic operators.
+
+# 6. Set your name and age variables in one line with multiple assignment
+
+# 7. Create two strings and then create a third variable combining both of the two original strings.
+
+# 8. Create a string and print the fourth letter of the word.
+
+# 9. Create a string and print the letters from index 0 to 5.
+
+# 10. Create a variable and print all the letters from the first index until the end.
