c893bd308bd6b149bf9f15719a6979bcba6d857d
arjunmini777 <arjunmini777@gmail.com>
2025-10-27 20:35:53 -0400

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,1 +1,18 @@
+# Write a script that adds 15 and 30 and divides the sum by 2. Print out the answer.
+a = 15
+b = 30
+c = (a + b)/2
+print(c)
 
+# Initialize two variables and use arithmetic operators to add, subtract, multiply, divide, and find the remainder.
+a = 20
+b = 10
+print(f"adding a + b is equal to {a+b}")
+print(f"subtracting a - b is equal to {a-b}")
+print(f"multiplying a * b is equal to {a*b}")
+print(f"dividing a / b is equal to {a/b}")
+print(f"The remainder of a % b is equal to {a%b}")
+
+# Create a variable called name and store your name in it as a string.
+name = "Arjun"
+print(name)
