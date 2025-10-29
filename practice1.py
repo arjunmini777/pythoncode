3ae9b9b76c74ff75d4e05836818ff3fd552e06f9
arjunmini777 <arjunmini777@gmail.com>
2025-10-28 20:35:41 -0400

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -18,15 +18,29 @@ name = "Arjun"
 print(name)
 
 # 4. Create three variables in one line and assign each one a different food item.
+a, b, c = "Apple", "Banana", "Carrots"
+print(a, b, c)
 
 # 5. Print out "Hello" ten times using arithmetic operators.
+print("hi"*10)
 
 # 6. Set your name and age variables in one line with multiple assignment
+Name, Age = "Arjun", 12
 
 # 7. Create two strings and then create a third variable combining both of the two original strings.
+Str1 = "Hello"
+Str2 = "world"
+Str3 = Str1 + Str2
+print(Str3)
 
 # 8. Create a string and print the fourth letter of the word.
+x = "Banana"
+print(x[3:4])
 
 # 9. Create a string and print the letters from index 0 to 5.
+y = "computer"
+print(y[0:5])
 
 # 10. Create a variable and print all the letters from the first index until the end.
+z = "zebra"
+print(z[:])
