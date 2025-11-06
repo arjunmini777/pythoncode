dd139ee7dc1cb7ddd0e766a0d331f4a5229229fa
Arjun <arjunmini777@gmail.com>
2025-11-05 20:20:42 -0500

    Fixed an error

--- tup_practice.py
+++ tup_practice.py
@@ -8,6 +8,6 @@ tup_3 = (6, 7, 8, 9, 10)
 tup_4 = tup_2 + tup_3
 print(f"The numbers from 1 to 10 are => {tup_4}")
 
-# now try del a tuple
+# now try deleting a tuple
 tup_5 = ("Billy", "James")
 del tup_5
