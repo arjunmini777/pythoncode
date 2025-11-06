7d0a2133c74acf278fe43a9c5c60e1e2d732de46
Arjun <arjunmini777@gmail.com>
2025-11-05 20:16:58 -0500

    another file with more work

---
+++ tup_practice.py
@@ -0,0 +1,13 @@
+# create a tuple with names of students
+tup_1 = ("Margret", "Billy", "Jeff")
+print(f"My classmates are => {tup_1} ")
+
+# add 2 tuples together
+tup_2 = (1, 2, 3, 4, 5)
+tup_3 = (6, 7, 8, 9, 10)
+tup_4 = tup_2 + tup_3
+print(f"The numbers from 1 to 10 are => {tup_4}")
+
+# now try del a tuple
+tup_5 = ("Billy", "James")
+del tup_5
