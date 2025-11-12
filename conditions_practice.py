a34eca3923bd36e5fd037cc05a1467e4b98085d8
Arjun <arjunmini777@gmail.com>
2025-11-11 19:05:38 -0500

    New file

---
+++ conditions_practice.py
@@ -0,0 +1,28 @@
+# Conditional statements 
+x = 20
+if x > 20:
+    print("x is greater than 20")
+elif x < 20:
+    print("x is smaller than 20")
+elif x == 20:
+    print("x is equal to 20")
+
+
+y = 65
+if y > 65:
+    print("y is greater than 65")
+else:
+    print("y is less than or equal to 65")
+
+# Boolean
+z = True
+if z:
+    print("Z: I like dogs")
+else:
+    print("Z: I hate dogs")
+
+a = False
+if a:
+    print("A: I like cats")
+else:
+    print("A: I dislike cats")
